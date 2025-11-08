7410caee747644110263e761080ffe3af3ccee19
Mariya_Suchkova <mashakraxmal@gmail.com>
2025-11-08 18:49:41 +0300

    bug fixesfile error correction

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,14 @@
 from typing import Union
-from mask import *
+from mask import get_mask_account, get_mask_card_number
 
-def mask_account_card(card: Union[str])-> Union[str]:
-    """ Функция для маскировки счета или карты"""
+
+def mask_account_card(card: Union[str]) -> Union[str]:
+    """Функция для маскировки счета или карты"""
 
     name_card = []
-    list_card = card.split(sep=' ',maxsplit=3)
+    list_card = card.split(sep=" ", maxsplit=3)
     for element in list_card:
-        if element.isdigit() == True:
+        if element.isdigit() is True:
             number_card = element
         else:
             name_card.append(element)
@@ -19,17 +20,17 @@ def mask_account_card(card: Union[str])-> Union[str]:
     else:
         mask_number_card = get_mask_card_number(number_card)
 
-    full_name_card = ' '.join(name_card)
+    full_name_card = " ".join(name_card)
 
-    return f'{full_name_card} {mask_number_card}'
+    return f"{full_name_card} {mask_number_card}"
 
-def get_date(date_time: Union[str])-> Union[str]:
+
+def get_date(date_time: Union[str]) -> Union[str]:
     """Функция для вывода даты"""
 
-    separation_date_time = date_time.split(sep='T', maxsplit=1)
-    reverse_date = separation_date_time[0].split(sep='-', maxsplit=3)
+    separation_date_time = date_time.split(sep="T", maxsplit=1)
+    reverse_date = separation_date_time[0].split(sep="-", maxsplit=3)
     coup_date = reverse_date[::-1]
-    date = '.'.join(coup_date)
+    date = ".".join(coup_date)
 
     return date
-
