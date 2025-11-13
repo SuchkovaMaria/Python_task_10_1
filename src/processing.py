da22968013fff303f07640dba3b94b07d3cd17a2
Mariya_Suchkova <mashakraxmal@gmail.com>
2025-11-13 21:40:56 +0300

    delit test filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -11,18 +11,6 @@ def filter_by_state(list_of_dict: Union[list], entered_state: Union[str]="EXECUT
     return filter_list
 
 
-i = filter_by_state(
-    [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ],
-    "CANCELED",
-)
-print(i)
-
-
 def sort_by_date(list_of_dict: Union[list], sort_order: Union[bool]=True) -> Union[list]:
     """Функция сортирует список словарей по дате"""
     sort_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=sort_order)
