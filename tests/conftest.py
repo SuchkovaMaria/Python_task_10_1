10c09790287460b6052ea2b7289200638ba4e33f
Mariya_Suchkova <mashakraxmal@gmail.com>
2025-11-23 16:04:07 +0300

    add test_processing.py fixed situations of entering incorrect data in processing.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,29 @@ def account_card_3():
 def account_card_4():
     return 'Visa Platinum 700079228960636164757636'
 
+@pytest.fixture
+def test_list_3():
+    return  [
+            {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+        ]
+
+@pytest.fixture
+def test_list_sort_date_2():
+    return [
+            {"id": 41428829, "state": "EXECUTED", "date": "T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "T08:21:33.419441"},
+        ]
+
+@pytest.fixture
+def test_list_sort_date_3():
+    return [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
---
+++ tests/test_processing.py
@@ -0,0 +1,77 @@
+from src.processing import filter_by_state, sort_by_date
+import pytest
+
+
+def test_filter_by_state_1():
+    assert filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    ) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state_2():
+    assert filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        "CANCELED",
+    ) == (
+        [
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+
+
+def test_filter_by_state_3(test_list_3):
+    assert filter_by_state(test_list_3) == "В списке отсутствует нужный статус"
+
+
+def test_sort_by_date_1():
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    ) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_sort_by_date_2(test_list_sort_date_2):
+    assert sort_by_date(test_list_sort_date_2) == "Невернно введены данные"
+
+
+def test_sort_by_date_3(test_list_sort_date_3):
+    assert sort_by_date(test_list_sort_date_3) == "Отсутствуют даты"
+
+
+def test_sort_by_date_4():
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    ) == [
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T21:27:25.241689"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
