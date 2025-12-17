2260dacbf0f7d8a420a257cf557af950f8edb340
Mariya_Suchkova <mashakraxmal@gmail.com>
2025-12-18 00:03:53 +0300

    add logging utils.py and mask.py

--- src/mask.py
+++ src/mask.py
@@ -1,4 +1,15 @@
+import datetime
+import logging
+import os
 from typing import Union
+
+os.chdir(r'C:\\Users\\Rabota\\PycharmProjects\\Python_task')
+logger = logging.getLogger('mask')
+logger.setLevel(logging.DEBUG)
+file_handler_mask = logging.FileHandler('logs/mask.log', mode='w', encoding='utf-8')
+file_formatter_mask = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler_mask.setFormatter(file_formatter_mask)
+logger.addHandler(file_handler_mask)
 
 
 def get_mask_account(number_account: Union[str]) -> Union[str]:
@@ -6,25 +17,34 @@ def get_mask_account(number_account: Union[str]) -> Union[str]:
 
     new_number_account = []
     mask_number_account = []
+    logger.debug('Созданы пустые вспомогательные списки: new_number_account, mask_number_account')
 
-    if number_account.isalpha() is False:
-        for number in number_account:
-            new_number_account.append(number)
+    try:
+        if number_account.isalpha() is False:
+            logger.debug('Номер счета не содержит букв')
+            for number in number_account:
+                new_number_account.append(number)
 
-        lenght_new_number_account = len(new_number_account)
-        if lenght_new_number_account == 20:
-            for num in range(len(new_number_account)):
-                index_num = num
-                if 14 <= index_num <= 15:
-                    mask_number_account.append("*")
-                elif index_num >= 16:
-                    mask_number_account.append(new_number_account[num])
-            mask_number_account_str = "".join(mask_number_account)
-            return mask_number_account_str
+            lenght_new_number_account = len(new_number_account)
+            logger.debug('Маскировка номера счета')
+            if lenght_new_number_account == 20:
+                for num in range(len(new_number_account)):
+                    index_num = num
+                    if 14 <= index_num <= 15:
+                        mask_number_account.append("*")
+                    elif index_num >= 16:
+                        mask_number_account.append(new_number_account[num])
+                mask_number_account_str = "".join(mask_number_account)
+                logger.info('Вывод замаскированного номера счета')
+                return mask_number_account_str
+            else:
+                logger.debug('Вывод сообщения о неверно введенных данных')
+                return "Невернно введены данные"
         else:
-            return "Невернно введены данные"
-    else:
-        return "Введен неверный номер счета"
+            logger.debug('Вывод сообщения о неверно введенного номер счета')
+            return "Введен неверный номер счета"
+    except Exception as e:
+        logger.error(f'Произошла ошибка: {e}')
 
 
 def get_mask_card_number(number_card: Union[str]) -> Union[str]:
@@ -32,34 +52,44 @@ def get_mask_card_number(number_card: Union[str]) -> Union[str]:
 
     new_number_card = []
     mask_card_number = []
+    logger.debug('Созданы пустые вспомогательные списки: new_number_card, mask_card_number')
     count = 0
+    logger.debug('Обнуление счетчика count')
 
-    if number_card.isalpha() is False:
-        lenght_number_card = len(number_card)
-        if lenght_number_card == 16:
-            for number in number_card:
-                new_number_card.append(number)
+    try:
+        if number_card.isalpha() is False:
+            logger.debug('Номер карты не содержит букв')
+            lenght_number_card = len(number_card)
+            if lenght_number_card == 16:
+                logger.debug('Маскировка номера карты')
+                for number in number_card:
+                    new_number_card.append(number)
 
-            new_number_card.insert(4, " ")
-            new_number_card.insert(9, " ")
-            new_number_card.insert(14, " ")
+                new_number_card.insert(4, " ")
+                new_number_card.insert(9, " ")
+                new_number_card.insert(14, " ")
 
-            for num_card in new_number_card:
-                if num_card != " ":
-                    if count < 7:
+                for num_card in new_number_card:
+                    if num_card != " ":
+                        if count < 7:
+                            mask_card_number.append(num_card)
+                            count += 1
+                        elif 7 <= count <= 13:
+                            mask_card_number.append("*")
+                            count += 1
+                        elif count > 14:
+                            mask_card_number.append(num_card)
+                    elif num_card == " ":
                         mask_card_number.append(num_card)
                         count += 1
-                    elif 7 <= count <= 13:
-                        mask_card_number.append("*")
-                        count += 1
-                    elif count > 14:
-                        mask_card_number.append(num_card)
-                elif num_card == " ":
-                    mask_card_number.append(num_card)
-                    count += 1
-            mask_card_number_str = "".join(mask_card_number)
-            return mask_card_number_str
+                mask_card_number_str = "".join(mask_card_number)
+                logger.info('Вывод замаскированного номера карты')
+                return mask_card_number_str
+            else:
+                logger.debug('Вывод сообщения о неверно введенных данных')
+                return "Невернно введены данные"
         else:
-            return "Невернно введены данные"
-    else:
-        return "Введен неверный номер карты"
+            logger.debug('Вывод сообщения о неверно введенного номера карты')
+            return "Введен неверный номер карты"
+    except Exception as e:
+        logger.error(f'Произошла ошибка: {e}')
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,15 @@
+import datetime
 import json
+import logging
+import os
+
+os.chdir(r'C:\\Users\\Rabota\\PycharmProjects\\Python_task')
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler_utils = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
+file_formatter_utils = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler_utils.setFormatter(file_formatter_utils)
+logger.addHandler(file_handler_utils)
 
 
 def convertions_json_in_list(path_to_file):
@@ -6,13 +17,19 @@ def convertions_json_in_list(path_to_file):
     try:
         with open(path_to_file, "r", encoding="utf-8") as file:
             file_operations = file.read()
+        logger.debug(f'Функция открыла файл по  пути: {path_to_file}')
         list_transactions = json.loads(file_operations)
+        logger.info('Данные из файла переведены в Python-объект')
     except FileNotFoundError:
+        logger.error('Файл не найден')
         print("Файл не найден")
         list_transactions = []
         return list_transactions
     if isinstance(list_transactions, list) is False:
+        logger.info(f'Данные в файле {list_transactions} не являются списком')
         list_transactions = []
     elif list_transactions == []:
+        logger.info(f'Список {list_transactions} пуст')
         list_transactions = []
+    logger.debug('Функция возвращает список транзакций')
     return list_transactions
